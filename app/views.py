3fdd9238490571fa3803497fd7216baad894678c
Kennard Elliott <kennard.elliott@mymona.uwi.edu>
2019-02-18 04:53:29 +0000

    Final Changes

--- app/views.py
+++ app/views.py
@@ -7,7 +7,8 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -23,7 +24,11 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
-
+    
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    """Render the websites's contact page."""
+    return render_template('contact.html')
 
 ###
 # The functions below should be applicable to all Flask apps.
